3120009a4e773992452aee26101fa3abfae85cf2
Kedari Teja <kedariteja1999@gmail.com>
2021-06-27 17:07:48 +0530

    commit jwt

--- SpringbootJWT/src/main/java/com/vm/spjwt/SecurityConfig.java
+++ SpringbootJWT/src/main/java/com/vm/spjwt/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
+		
 		//super.configure(http);
 		http.csrf().disable()
 		.authorizeRequests().antMatchers("/authenticate").permitAll()
